5422e7cde2f08eab25fb041b56f16bd3cccd5647
telpvn <telpvn@mail.ru>
2024-04-24 17:48:49 +0400

    With a loop of while

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         //todo Тут увольняем айтишников
-        for (int i = 2; i <= 50; i += 2) {
+        int i = 2;
+        while (i <= 50) {
             System.out.println("Уволен айтишник с id " + i);
+            i += 2;
         }
+
     }
 }
