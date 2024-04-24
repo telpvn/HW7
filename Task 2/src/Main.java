f30107acd4968a69d545b5a5b8b0880f06bd8ae7
telpvn <telpvn@mail.ru>
2024-04-24 17:44:13 +0400

    With a loop of for

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         //todo Тут увольняем айтишников
+        for (int i = 2; i <= 50; i += 2) {
+            System.out.println("Уволен айтишник с id " + i);
+        }
     }
 }
